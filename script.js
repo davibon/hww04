6fca277c543f5222265dd5d5d5635f782de74e9d
davi2137 <davi2137@gmail.com>
2017-06-22 16:11:55 +0800

    add

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ d3.json("invoice-taipei.json", function (dataSet) {
 });
 
 function bindInvoice(dataSet) {
-    var selection = d3.select("svg")
+    var selection = d3.select("svg#invoice")
         .selectAll("rect")
         .data(dataSet);
     selection.enter().append("rect");
     selection.exit().remove();
 
-    var seleText = d3.select("svg")
+    var seleText = d3.select("svg#invoice")
         .selectAll("text")
         .data(dataSet);
     seleText.enter().append("text");
@@ -36,7 +36,7 @@ function renderInvoice(dataSet) {
 
 
 
-    d3.selectAll("rect")
+    d3.selectAll("#invoice>rect")
         .attr({
             x: 250,
             y: function (d, i) {
@@ -50,7 +50,7 @@ function renderInvoice(dataSet) {
             fill: "navy"
         });
 
-    d3.selectAll("text")
+    d3.selectAll("#invoice>text")
         .attr({
             x: 0,
             y: function (d, i) {
@@ -77,3 +77,89 @@ function renderInvoice(dataSet) {
         .attr("transform", "translate(" + (250 - 10) + "," + (400 - 50) + ")")
         .call(xAxis);
 }
+
+
+
+
+        //速度函式橫桿
+
+        var svgBall = d3.select("svg#sball");
+
+        svgBall.append("g").append("rect").attr({
+            fill: "white",
+            width: "100%",
+            height: "100%"
+        });
+
+        //--------------------------------
+        var easeArr = ["linear", "quad", "cubic", "sin", "exp", "circle", "elastic", "back", "bounce"];
+
+        bindBall(easeArr);
+        renderBall();
+
+        function bindBall(dataSet) {
+            var selection_circle = d3.select("svg#sball").selectAll("#sball>circle").data(dataSet);
+            var selection_text = d3.select("svg#sball").selectAll("#sball>text").data(dataSet);
+            var selection_line = d3.select("svg#sball").selectAll("#sball>line").data(dataSet);
+
+            // 初始化設定
+            selection_line.enter().append("line").attr({
+                x1: 100,
+                y1: function(d, i) {
+                    return 20 + i * 35;
+                },
+                x2: 300,
+                y2: function(d, i) {
+                    return 20 + i * 35;
+                },
+                stroke: "lightgreen"
+            }).text(function(d) {
+                return d;
+            });
+            selection_line.exit().remove();
+
+            selection_circle.enter().append("circle").attr({
+                cx: 100,
+                cy: function(d, i) {
+                    return 20 + i * 35;
+                },
+                r: 15,
+                fill: "gold"
+            });
+            selection_circle.exit().remove();
+
+            selection_text.enter().append("text").attr({
+                x: 5,
+                y: function(d, i) {
+                    return 25 + i * 35;
+                },
+                fill: "black"
+            }).text(function(d) {
+                return d;
+            });
+            selection_text.exit().remove();
+
+
+        }
+
+        function renderBall() {
+
+            //需要動畫的設定
+            d3.select("#sball").selectAll("#sball>circle")
+                .on("click", function(d) {
+                    console.log(d3.select(this).attr("cx"));
+                    var nowCx = d3.select(this).attr("cx");
+                    if (nowCx > 100) {
+                        nowCx = 100;
+                    } else {
+                        nowCx = 300;
+                    }
+                    d3.select(this).transition()
+                        .ease(d)
+                        .attr({
+                            cx: nowCx
+                        });
+                });
+
+
+        }
